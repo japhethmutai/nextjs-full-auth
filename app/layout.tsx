0a0bd3614af54a7aba5a64906a40364f207f8654
japheth <japhemchirie@gmail.com>
2023-12-16 13:39:22 +0300

    organize imports from the same folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
-import Navbar from '@/components/common/Navbar'
-import Footer from '@/components/common/Footer'
+import { Navbar, Footer } from '@/components/common'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <div>
+          {children}
+        </div>
         <Footer />
       </body>
     </html>
