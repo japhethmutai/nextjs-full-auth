f802d9a72725d95b091b07028f29ec961f19b521
japheth <japhemchirie@gmail.com>
2023-12-16 14:57:34 +0300

    setup authslice to manage login state

--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/authSlice";
 
 export const makeStore = () =>
     configureStore({
-        reducer: {},
+        reducer: {
+            auth: authReducer
+        },
         devTools: process.env.NODE_ENV !== 'production'
     })
 
