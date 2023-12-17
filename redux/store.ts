6c83a8c05d3ec24ca1bcdbc0e6431f502ce60cac
japheth <japhemchirie@gmail.com>
2023-12-17 17:46:35 +0300

    correct error with store

--- redux/store.ts
+++ redux/store.ts
@@ -2,14 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./services/apiSlice";
 import authReducer from "./features/authSlice";
 
-export const makeStore = () =>
-    configureStore({
-        reducer: {
-            [apiSlice.reducerPath]: apiSlice.reducer,
-            auth: authReducer
-        },
-        devTools: process.env.NODE_ENV !== 'production'
-    })
+export const makeStore = () => configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authReducer
+    },
+    devTools: process.env.NODE_ENV !== 'production'
+})
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore['getState']>;
