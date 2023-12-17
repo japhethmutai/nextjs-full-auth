4c479d83605bc672bd232df7393bb5c35c7f1e60
japheth <japhemchirie@gmail.com>
2023-12-17 17:36:46 +0300

    create a spinner and add it to register page

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "react-toastify";
+import { Spinner } from "@/components/common";
 
 const Page = () => {
 	const router = useRouter();
@@ -144,7 +145,7 @@ const Page = () => {
 							type="submit"
 							className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						>
-							Sign up
+							{ isLoading ? <Spinner sm /> : 'Sign up' }
 						</button>
 					</div>
 				</form>
