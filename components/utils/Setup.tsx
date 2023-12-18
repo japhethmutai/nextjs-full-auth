f9282e677abb58272199d16b8ebccf67800eb5e8
japheth <japhemchirie@gmail.com>
2023-12-18 18:06:29 +0300

    load setup container to verify user login status on page load

--- components/utils/Setup.tsx
+++ components/utils/Setup.tsx
@@ -1,9 +1,27 @@
 'use-client';
 
+import { useEffect } from "react";
+import { useAppDispatch } from "@/redux/hooks";
+import { setAuth, finishInitialLoad } from "@/redux/features/authSlice";
+import { useVerifyMutation } from "@/redux/features/authApiSlice";
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 
 const Setup = () => {
+	const dispatch = useAppDispatch();
+	const [verify] = useVerifyMutation();
+
+	useEffect(() => {
+		verify(undefined)
+			.unwrap()
+			.then(() => {
+				dispatch(setAuth());
+			})
+			.finally(() => {
+				dispatch(finishInitialLoad());
+			})
+	}, [])
+
 	return <ToastContainer />
 }
 
