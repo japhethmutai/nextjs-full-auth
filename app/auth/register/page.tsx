2218d0a8e0798bc4fd321ce31d74cf8eb4b9d6bb
japheth <japhemchirie@gmail.com>
2023-12-18 11:52:02 +0300

    create hooks to refactor register form

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,44 +1,20 @@
 'use client';
 
-import { useRegisterMutation } from "@/redux/features/authApiSlice";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, FormEvent, useState } from "react";
-import { toast } from "react-toastify";
 import { Spinner } from "@/components/common";
+import { useRegister } from "@/hooks";
 
 const Page = () => {
-	const router = useRouter();
-	const [register, { isLoading }] = useRegisterMutation();
-
-	const [formData, setFormData] = useState({
-		first_name: '',
-		last_name: '',
-		email: '',
-		password: '',
-		re_password: '',
-	})
-
-	const { first_name, last_name, email, password, re_password } = formData;
-
-	const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = event.target;
-		setFormData({ ...formData, [name]: value })
-	}
-
-	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-
-		register({ first_name, last_name, email, password, re_password })
-			.unwrap()
-			.then(() => {
-				toast.success('Please check email to verify account.');
-				router.push('/auth/login')
-			})
-			.catch(() => {
-				toast.error('Failed to register account.')
-			})
-	}
+	const {
+		first_name,
+        last_name,
+        email,
+        password,
+        re_password,
+        isLoading,
+        onChange,
+        onSubmit
+	} = useRegister();
 
 	return (
 		<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
