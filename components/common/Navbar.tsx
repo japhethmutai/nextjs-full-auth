80984ea4dd3e5fda6fe47d1fcb7f9609ce43bb01
japheth <japhemchirie@gmail.com>
2023-12-18 19:54:33 +0300

    create navlinks component and use it in navbar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -6,11 +6,13 @@ import Link from 'next/link'
 import { useAppSelector, useAppDispatch } from '@/redux/hooks';
 import { useLogoutMutation } from '@/redux/features/authApiSlice';
 import { logout as setLogout } from '@/redux/features/authSlice';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
+import { NavLink } from '@/components/common'
 
 export default function Navbar() {
     const dispatch = useAppDispatch();
     const router = useRouter();
+    const pathname = usePathname();
     const { isAuthenticated } = useAppSelector(state => state.auth);
     const [logout] = useLogoutMutation();
 
@@ -25,12 +27,40 @@ export default function Navbar() {
             })
     }
 
-    const authLinks = (
-        <div>AUTH LINKS</div>
+    const isSelected = (path: string) => pathname === path ? true : false;
+
+    const authLinks = (isMobile: boolean) => (
+        <>
+            <NavLink
+                isSelected={ isSelected('/dashboard') }
+                isMobile={isMobile}
+                href='/dashboard'
+            >
+                Dashboard
+            </NavLink>
+            <NavLink isMobile={isMobile} onClick={handleLogout}>
+                Logout
+            </NavLink>
+        </>
     )
 
-    const guestLinks = (
-        <div>GUEST LINKS</div>
+    const guestLinks = (isMobile: boolean) => (
+        <>
+            <NavLink
+                isSelected={ isSelected('/auth/login') }
+                isMobile={isMobile}
+                href='/auth/login'
+            >
+                Login
+            </NavLink>
+            <NavLink
+                isSelected={ isSelected('/auth/register') }
+                isMobile={isMobile}
+                href='/auth/register'
+            >
+                Register
+            </NavLink>
+        </>
     )
 
     return (
@@ -52,13 +82,13 @@ export default function Navbar() {
                                 </Disclosure.Button>
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <Link className='text-gray-300 rounded-md px-3 py-2 font-medium' href='/'>
+                                <NavLink href='/' isBanner>
                                     Maxicom
-                                </Link>
+                                </NavLink>
                             </div>
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
-                                    { isAuthenticated ? authLinks : guestLinks }
+                                    { isAuthenticated ? authLinks(false) : guestLinks(false) }
                                 </div>
                             </div>
                         </div>
@@ -66,7 +96,7 @@ export default function Navbar() {
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
-                            { isAuthenticated ? authLinks : guestLinks }
+                            { isAuthenticated ? authLinks(true) : guestLinks(true) }
                         </div>
                     </Disclosure.Panel>
                 </>
