c8c16e49d002bd52fd71c15dec7a28e8f5930c76
japheth <japhemchirie@gmail.com>
2023-12-18 13:39:28 +0300

    setup login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,37 @@
+import { RegisterForm } from "@/components/forms";
+import Link from "next/link";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: 'Maxicom | Login',
+	description: 'Maxicom IT Consultancy login page'
+}
+
 const Page = () => {
 	return (
-		<main>
-			<h1>Login</h1>
-		</main>
+		<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+			<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+				<img
+					className="mx-auto h-10 w-auto"
+					src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+					alt="Full Auth"
+				/>
+				<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+					Sign in to your account
+				</h2>
+			</div>
+
+			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+				<RegisterForm />
+
+				<p className="mt-10 text-center text-sm text-gray-500">
+					Don&apos;t have an account?{' '}
+					<Link href="/auth/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+						Register here
+					</Link>
+				</p>
+			</div>
+		</div>
 	)
 }
 
