6ec1aa881050da83e8ba14335bed702cda93ec1f
japheth <japhemchirie@gmail.com>
2023-12-18 12:43:11 +0300

    refactor register form elements

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,21 +1,7 @@
-'use client';
-
+import { RegisterForm } from "@/components/forms";
 import Link from "next/link";
-import { Spinner } from "@/components/common";
-import { useRegister } from "@/hooks";
 
 const Page = () => {
-	const {
-		first_name,
-        last_name,
-        email,
-        password,
-        re_password,
-        isLoading,
-        onChange,
-        onSubmit
-	} = useRegister();
-
 	return (
 		<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 			<div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -30,101 +16,7 @@ const Page = () => {
 			</div>
 
 			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-				<form className="space-y-6" onSubmit={onSubmit}>
-					<div>
-						<label htmlFor="first_name" className="block text-sm font-medium leading-6 text-gray-900">
-							First Name
-						</label>
-						<div className="mt-2">
-							<input
-								id="first_name"
-								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								name="first_name"
-								type="text"
-								onChange={onChange}
-								value={first_name}
-								required
-							/>
-						</div>
-					</div>
-
-					<div>
-						<label htmlFor="last_name" className="block text-sm font-medium leading-6 text-gray-900">
-							Last Name
-						</label>
-						<div className="mt-2">
-							<input
-								id="last_name"
-								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								name="last_name"
-								type="text"
-								onChange={onChange}
-								value={last_name}
-								required
-							/>
-						</div>
-					</div>
-
-					<div>
-						<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-							Email Address
-						</label>
-						<div className="mt-2">
-							<input
-								id="email"
-								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								name="email"
-								type="email"
-								onChange={onChange}
-								value={email}
-								required
-							/>
-						</div>
-					</div>
-
-					<div>
-						<label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-							Password
-						</label>
-						<div className="mt-2">
-							<input
-								id="password"
-								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								name="password"
-								type="password"
-								onChange={onChange}
-								value={password}
-								required
-							/>
-						</div>
-					</div>
-
-					<div>
-						<label htmlFor="re_password" className="block text-sm font-medium leading-6 text-gray-900">
-							Confirm Password
-						</label>
-						<div className="mt-2">
-							<input
-								id="re_password"
-								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								name="re_password"
-								type="password"
-								onChange={onChange}
-								value={re_password}
-								required
-							/>
-						</div>
-					</div>
-
-					<div>
-						<button
-							type="submit"
-							className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-						>
-							{ isLoading ? <Spinner sm /> : 'Sign up' }
-						</button>
-					</div>
-				</form>
+				<RegisterForm />
 
 				<p className="mt-10 text-center text-sm text-gray-500">
 					Already have an account?{' '}
---
+++ components/forms/Form.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, FormEvent } from "react";
+import { Input } from '@/components/forms';
+import { Spinner } from "@/components/common";
+
+interface Config {
+	labelText: string;
+	labelId: string;
+	type: string;
+	value: string;
+	required?: boolean;
+}
+
+interface Props {
+	config: Config[];
+	isLoading: boolean;
+	btnText: string;
+	onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+	onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+const Form = ({ config, isLoading, btnText, onChange, onSubmit }: Props) => {
+	return (
+		<form className="space-y-6" onSubmit={onSubmit}>
+			{
+				config.map((input, index) => (
+					<Input
+						key={index}
+						labelId={input.labelId}
+						type={input.labelId}
+						onChange={onChange}
+						value={input.value}
+					>
+						{input.labelText}
+					</Input>
+				))
+			}
+			<div>
+				<button
+					type="submit"
+					className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+				>
+					{ isLoading ? <Spinner sm /> : `${btnText}` }
+				</button>
+			</div>
+		</form>
+	)
+}
+
+export default Form
