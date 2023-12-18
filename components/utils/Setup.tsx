ebdaa949a9bd59892f0c1a25df68e5641c683ea5
japheth <japhemchirie@gmail.com>
2023-12-18 18:15:30 +0300

    refactor setup code with use-verify hook

--- components/utils/Setup.tsx
+++ components/utils/Setup.tsx
@@ -1,26 +1,11 @@
 'use client';
 
-import { useEffect } from "react";
-import { useAppDispatch } from "@/redux/hooks";
-import { setAuth, finishInitialLoad } from "@/redux/features/authSlice";
-import { useVerifyMutation } from "@/redux/features/authApiSlice";
+import { useVerify } from "@/hooks";
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 
 const Setup = () => {
-	const dispatch = useAppDispatch();
-	const [verify] = useVerifyMutation();
-
-	useEffect(() => {
-		verify(undefined)
-			.unwrap()
-			.then(() => {
-				dispatch(setAuth());
-			})
-			.finally(() => {
-				dispatch(finishInitialLoad());
-			})
-	}, [])
+	useVerify();
 
 	return <ToastContainer />
 }
