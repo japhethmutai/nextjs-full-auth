432de2568b9bf0c9eafcd1fffca53f2506baeccc
japheth <japhemchirie@gmail.com>
2023-12-18 13:19:50 +0300

    implement the activation page to call backend api

--- app/activation/[uid]/[token]/page.tsx
+++ app/activation/[uid]/[token]/page.tsx
@@ -1,8 +1,45 @@
-const Page = () => {
+'use client';
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import { useActivationMutation } from "@/redux/features/authApiSlice";
+
+interface Props {
+	params: {
+		uid: string;
+		token: string;
+	}
+}
+
+const Page = ({ params }: Props) => {
+	const router = useRouter();
+	const [activation] = useActivationMutation();
+
+	useEffect(() => {
+		const { uid, token } = params;
+
+		activation({ uid, token })
+			.unwrap()
+			.then(() => {
+				toast.success('Account activated.')
+			})
+			.catch(() => {
+				toast.error('Failed to activate account')
+			})
+			.finally(() => {
+				router.push('/auth/login')
+			})
+	}, [])
+
 	return (
-		<main>
-			<h1>Activation</h1>
-		</main>
+		<div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+			<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+				<h1 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight to-gray-900'>
+					Activating your account...
+				</h1>
+			</div>
+		</div>
 	)
 }
 
