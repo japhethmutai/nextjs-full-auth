abfcaae0f2948bf122041c9c10f2e8c6454be185
japheth <japhemchirie@gmail.com>
2023-12-20 19:55:17 +0300

    password reset form component

---
+++ components/forms/PasswordResetForm.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useResetPassword } from '@/hooks';
+import { Form } from '@/components/forms';
+
+const PasswordResetForm = () => {
+    const { email, isLoading, onChange, onSubmit } = useResetPassword();
+
+	const config = [
+		{
+			labelText: 'Email address',
+			labelId: 'email',
+			type: 'email',
+			onChange,
+			value: email,
+			required: true,
+		},
+	];
+
+	return (
+		<Form
+			config={config}
+			isLoading={isLoading}
+			btnText='Request password reset'
+			onChange={onChange}
+			onSubmit={onSubmit}
+		/>
+	);
+}
+
+export default PasswordResetForm
