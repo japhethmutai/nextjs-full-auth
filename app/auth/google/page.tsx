a7cd29b640fb364629b85edf8a71c5d4d459da58
japheth <japhemchirie@gmail.com>
2023-12-21 20:44:55 +0300

    configure google login page

--- app/auth/google/page.tsx
+++ app/auth/google/page.tsx
@@ -1,9 +1,18 @@
-const Page = () => {
+'use client';
+
+import { useSocialAuthenticateMutation } from '@/redux/features/authApiSlice';
+import { useSocialAuth } from '@/hooks';
+import { Spinner } from '@/components/common';
+
+const page = () => {
+	const [googleAuthenticate] = useSocialAuthenticateMutation();
+	useSocialAuth(googleAuthenticate, 'google-oauth2');
+
 	return (
-		<main>
-			<h1>Google</h1>
-		</main>
-	)
+		<div className='my-8'>
+			<Spinner lg />
+		</div>
+	);
 }
 
-export default Page
+export default page
