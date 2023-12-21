3b94eedf9b9d8abadf8c86415e9dfd0d557cc284
japheth <japhemchirie@gmail.com>
2023-12-21 20:26:11 +0300

    add social auth buttons to login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,6 @@
 import { LoginForm } from "@/components/forms";
 import Link from "next/link";
+import { SocialButtons } from '@/components/common';
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ const Page = () => {
 
 			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 				<LoginForm />
+				<SocialButtons />
 
 				<p className="mt-10 text-center text-sm text-gray-500">
 					Don&apos;t have an account?{' '}
--- hooks/index.ts
+++ hooks/index.ts
@@ -1,6 +1,6 @@
 export { default as useLogin } from './use-login'
 export { default as usePasswordReset } from './use-password-reset'
 export { default as usePasswordResetConfirm } from './use-password-reset-confirm'
-export { default as useRegister } from './use-register'
+export { default as useRegister } from './use-register';
 export { default as useSocialAuth } from './use-social-auth';
-export { default as useVerify } from './use-verify'
+export { default as useVerify } from './use-verify';
